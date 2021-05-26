e9accf8e07753405b2db81f6e5f4f88e84f0abb8
Fran <f.amarolopes@hotmail.com>
2021-05-26 11:59:15 +0200

    nouvelle modif écran indications

--- app/src/main/java/com/example/detectcolor/EcranInfoCouleurs.java
+++ app/src/main/java/com/example/detectcolor/EcranInfoCouleurs.java
@@ -1,7 +1,12 @@
 package com.example.detectcolor;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
+import android.widget.TextView;
 
 public class EcranInfoCouleurs extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class EcranInfoCouleurs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecran_info_couleurs);
+
+       //TextView textView = findViewById(R.id.text);
+
+       // String text = "Les couleurs opposées correspondent à la couleur complémentaire et deux autres proches, opposées à couleur sélectionnée sur le cercle chromatique. ";
+
+      //  SpannableString ss = new SpannableString(text);
+
+      //  StyleSpan boldSpan =  new StyleSpan(Typeface.BOLD);
+
+      //  ss.setSpan(boldSpan, 0, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+      //  TextView.setText(ss);
+
     }
 }
