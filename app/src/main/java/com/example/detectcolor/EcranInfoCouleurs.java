0c18af130cd2dcc529c46c42dcb3b60d7cf8f532
Fran <f.amarolopes@hotmail.com>
2021-05-26 15:59:42 +0200

    modif bouton pour aller en arrière

--- app/src/main/java/com/example/detectcolor/EcranInfoCouleurs.java
+++ app/src/main/java/com/example/detectcolor/EcranInfoCouleurs.java
@@ -1,31 +1,34 @@
 package com.example.detectcolor;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class EcranInfoCouleurs extends AppCompatActivity {
+
+    private ImageButton retour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecran_info_couleurs);
 
-       //TextView textView = findViewById(R.id.text);
+        this.retour = (ImageButton)findViewById(R.id.BoutonRetour);
 
-       // String text = "Les couleurs opposées correspondent à la couleur complémentaire et deux autres proches, opposées à couleur sélectionnée sur le cercle chromatique. ";
-
-      //  SpannableString ss = new SpannableString(text);
-
-      //  StyleSpan boldSpan =  new StyleSpan(Typeface.BOLD);
-
-      //  ss.setSpan(boldSpan, 0, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-      //  TextView.setText(ss);
-
+       retour.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent otherActivity = new Intent(getApplicationContext(), ResultatActivity.class);
+               startActivity(otherActivity);
+               finish();
+           }
+       });
     }
 }
