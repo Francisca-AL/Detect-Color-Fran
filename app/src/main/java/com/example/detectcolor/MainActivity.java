5aad4bafd0be14ac314a293f4493cd4278480a3a
Fran <f.amarolopes@hotmail.com>
2021-05-27 14:00:39 +0200

    Tapbar fonctionnel!!

--- app/src/main/java/com/example/detectcolor/MainActivity.java
+++ app/src/main/java/com/example/detectcolor/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.detectcolor;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem Item) {
+                    Fragment selectedFragment =  null;
+
+                    switch (Item.getItemId()){
+                        case R.id.nav_home :
+                            selectedFragment = new HomeFragment() ;
+                            break;
+
+                        case R.id.nav_search :
+                            selectedFragment = new SearchFragment();
+                            break;
+
+                        case R.id.nav_settings :
+                            selectedFragment = new SettingsFragment();
+                            break;
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
 }
 
 /*
